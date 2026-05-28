b084ff3503151be3caeeb98ed878e862fa3505ba
SamarthMistrySM <samarthmistry311@gmail.com>
2026-05-28 23:54:34 +0530

    added versel config

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,31 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/task_tracking";
+const MONGO_URI = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
 async function connectDatabase() {
-  await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log("Connected to MongoDB");
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+      console.log("Connected to MongoDB");
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
 
 module.exports = { connectDatabase };
